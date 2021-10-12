fb66b80ecedd3e746f337ecc12c3c4ed9d529ed1
unknown <mimi.raykova@gmail.com>
2021-10-12 13:05:40 +0300

    initial commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const API = "https://new-desire.herokuapp.com"; //http://localhost:9999
+
+export const STRIPE_PUBLISHABLE ="pk_test_51JdAdCF4Yqr45yd9fJBb7uulrO4Q53z6yUmxWbmKZwij2gbxlIzXZpZCdHzp6wD2jOyzfiO67n5BEKDbKCYwcF3Z00LU9IjWkv";
+
+// export const API = process.env.NODE_ENV === 'production'
+// ? 'https://new-desire.herokuapp.com/'
+// : 'http://localhost:9999';
+
+// export default PAYMENT_SERVER_URL;
+// const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
+// ? 'pk_live_MY_PUBLISHABLE_KEY'
+// : 'pk_test_MY_PUBLISHABLE_KEY';
+
+// export default STRIPE_PUBLISHABLE;
---
+++ src/utils/getProductService.js
@@ -0,0 +1,35 @@
+import { API } from "../config";
+
+export const getCategories = () => {
+  return fetch(`${API}/api/category`)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
+export const getProducts = () => {
+  return fetch(`${API}/api/product`)
+    .then((res) => {
+      console.log("ot getProducts res : " + JSON.stringify(res))
+      return res.json()
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+export const getProduct = (id) => {
+  return fetch(`${API}/api/product/${id}`)
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getProductsByCategory = (queryParam) => {
+
+  return fetch(`${API}/api/category/${queryParam}`)
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+
